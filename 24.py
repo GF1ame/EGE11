17abfc24a7e749bf6a93f59e83a2a372d85b0c2e
GFlame <Gol_m72@icloud.com>
2026-03-20 23:17:18 +0500

    new 24 type solve added

--- 24.py
+++ 24.py
@@ -1,8 +1,35 @@
 f = open('Files/24_tasks/24_28007.txt').readline()
-
-for i in range(len(f)):
-    for x in range(i,len(f)-1):
-        print(f[x])
+f = f.replace(")(",") (")
+x = f.split()
+current = ""
+maximum = ""
+newList = []
+for i in x:
+    i = i.replace("("," ").replace(")"," ")
+    splitted = i.split()
+    for z in range(len(splitted)):
+        if not splitted[z][0].isdigit():
+            splitted[z] = ""
+            continue
+    newList += splitted
+for i in newList:
+    if i == "":
+        if len(current) > len(maximum):
+            maximum = current
+        current = ""
+        continue
+    a = str(i).replace("+"," ").replace("-"," ").split()
+    if len(a) != 2: continue
+    if int(a[0]) % 5 == 0 or int(a[1]) % 5 != 0 or a[0][0] == "0" or a[1][0] == "0":
+        if len(current) > len(maximum):
+            maximum = current
+        current = ""
+        continue
+    current += f'({a[0]}+{a[1]})'
+print(len(maximum),maximum)
+# for i in range(len(f)):
+#     for x in range(i,len(f)-1):
+#         print(f[x])
     
 
 #--------------------1975--------------------
