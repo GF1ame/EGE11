a49e3a588dd3a55b6dc88cebc43c96328b1842bb
Максим Голыш <gol_m72@icloud.com>
2026-05-16 22:04:54 +0500

    new 25th task solve

--- 5.py
+++ 5.py
@@ -59,3 +59,15 @@ for N in range(1,1000):
         a.append(R)
 print(max(a))
 #---------------------------------------------
+
+a = []
+for N in range(1,1000):
+    binary = bin(N)[2:]
+    if binary.count("1") % 2 == 0:
+        binary = "10" + binary[2:] + "0"
+    else:
+        binary = "11" + binary[2:] + "1"
+    R = int(binary,2)
+    if R < 19:
+        a.append(N)
+print(max(a))
