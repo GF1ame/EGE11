d5e20612c4f1893e44e146d431da23e998c9495d
GFlame <Gol_m72@icloud.com>
2026-05-21 17:03:26 +0500

    debug

--- 24.py
+++ 24.py
@@ -1,3 +1,5 @@
+
+import time
 #--------------------28007--------------------
 f = open('Files/24_tasks/24_28007.txt').readline()
 f = f.replace(")(",") (")
@@ -81,21 +83,70 @@ for i in "BCD": n = n.replace(i,"-")
 
 mx = 1
 
-for l in range(len(n)):
-    for r in range(l+mx,len(n)):
-        s = n[l:r+1]
-        if s[:2] == "+-" and s[-2:] == "+-":
-            if "--" in s or "++" in s: break
-            if "+-" in s:
-                mx = max(mx, len(s))
-print(mx)
-        
 
-        
+#--------------------27777--------------------
+from string import *
+
+n = open("Files/24_tasks/24_27777.txt").readline()
+
+for i in (ascii_uppercase + "0123456789"):
+    if i.isdigit() or i == "A" or i == "B": continue
+    n = n.replace(i," ")
+print(max([len(i) for i in n.split()]))
+
+#--------------------27634--------------------
+
+n = open("Files/24_tasks/24_27634.txt").readline()
+
+
+m = 2000
+a = ""
+zCounter = 0
+bestLen = 99999
+
+start1 = time.perf_counter()
+
+
+for l in range(len(n)):
+    for r in range(m+l,l,-1):
+        a = n[l:r]
+        if a.count("Z") < 270:break
+        else: m = len(a) 
+print(m)
+
+end1 = time.perf_counter()
+print(f"Время выполнения: {end1 - start1:.6f} секунд")
+
+start2 = time.perf_counter()
+
+for l in range(len(n)):
+    a += n[l]
+    if a[-1] == "Z": zCounter += 1        
+    while zCounter >= 270:
+        bestLen = min(bestLen,len(a))
+        if a[0] == "Z": zCounter -= 1
+        a = a[1:]
+print(bestLen)
+
+end2 = time.perf_counter()
+print(f"Время выполнения: {end2 - start2:.6f} секунд")
+
 #--------------------------------------------
 
-# import time
+l = 0
+m = 0
+n = open("Files/24_tasks/24-241.txt").readline()
+for r in range(len(n)):
+    if n[r] == "O" and n[l] == "O":
+        while n[l:r].count("F") > 2:
+            l += 1
+        m = max(m,r-l+1)
+    if n[l]!="O": l += 1
+print(m)
 
-# start = time.perf_counter()
-# end = time.perf_counter()
-# print(f"Время выполнения: {end - start:.6f} секунд")
+
+
+
+         
+#--------------------------------------------
+
